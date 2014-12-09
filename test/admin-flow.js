d4454697640be703893cb0359a680bd2f7d41c01
Andrew Hobden <andrew@hoverbear.org>
2014-12-09 09:05:45 -0800

    Better stats.

--- test/admin-flow.js
+++ test/admin-flow.js
@@ -1,7 +1,7 @@
 /**
- * The goal of this test suite is to ensure admin functionality is working as
- * intended.
- */
+* The goal of this test suite is to ensure admin functionality is working as
+* intended.
+*/
 var util = require('./util');
 
 /** Config */
@@ -33,6 +33,8 @@ casper.test.begin('Admin page seems populated', function suite(test) {
         test.assertExists("a[href='/admin/facilitators']", "Link to facilitators present");
         test.assertExists("a[href='/admin/members']", "Link to members present");
         test.assertExists("a[href='/admin/groups']", "Link to groups present, and active.");
+        test.assertExists("table", "Table exists");
+        test.assertExists("table tbody tr", "Row exists in table.");
     }).run(function () {
         test.done();
     });
@@ -52,6 +54,23 @@ casper.test.begin('Statistics page seems populated', function suite(test) {
         test.assertExists("#ageChart");
         test.assertExists("#regionChart");
         test.assertExists("#dateChart");
+    }).run(function () {
+        test.done();
+    });
+});
+
+/** Workshops */
+casper.test.begin('Able to create workshops', function suite(test) {
+    casper.start(host + '/register', function () {
+        this.fill("form[action='/login']", { email: util.admin.email, password: util.admin.password }, true);
+        // Success brings us to account page.
+    }).waitForUrl(/account$/, function () {
+        this.click("a[href='/workshops']");
+    }).waitForUrl(/workshops$/, function () {
+        // Should be nothing there... yet.
+        test.assertExists('.alert.alert-success.lead', 'Notice about availability present.');
+        // As an admin, we should have a button to create a workshop.
+        test.assertExists('a[href="/workshop/create"]', 'Link to create workshop present.');
     }).run(function () {
         test.done();
     });
